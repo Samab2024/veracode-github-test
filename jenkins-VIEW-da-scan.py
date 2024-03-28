a7746a43adbe96a8354ef7fd57196ef0c6b8cbf4
Samab2024 <158163459+Samab2024@users.noreply.github.com>
2024-03-28 18:37:43 +0530

    Update jenkins-VIEW-da-scan.py

--- jenkins-VIEW-da-scan.py
+++ jenkins-VIEW-da-scan.py
@@ -111,11 +111,15 @@ print("Looking for Application: " + app_name )
 res = prepared_request('GET', 'https://api.veracode.com/appsec/v1/applications' + '?name=' + app_name)
 response = res.json()
 print(res.json())
-try:
-#    app_id = response['_embedded']['applications'][0]['id']
+    app_id = response['_embedded']['applications'][0]['id']
     app_guid = response['_embedded']['applications'][0]['guid']
+    app_name = response['_embedded']['applications'][0]['profile']['name']
+print('Output: ' + app_id + ' ' + app_guid + ' ' + app_name)
+#try:
+#    app_id = response['_embedded']['applications'][0]['id']
+#    app_guid = response['_embedded']['applications'][0]['guid']
 #    app_name = response['_embedded']['applications'][0]['profile']['name']
-    print('App ID for ' + app_name + ' is ' + app_id + '.' 'Application GUID is ' + app_guid + '.')
-except: 
-    print("Could not find Application")
-    sys.exit(1)
+#    print('App ID for ' + app_name + ' is ' + app_id + '.' 'Application GUID is ' + app_guid + '.')
+#except: 
+#    print("Could not find Application")
+#    sys.exit(1)
