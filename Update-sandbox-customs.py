e21523ba7621e57b83b23c90932db25ef02018c0
Samab2024 <158163459+Samab2024@users.noreply.github.com>
2024-04-22 13:04:34 +0530

    Update Update-sandbox-customs.py

--- Update-sandbox-customs.py
+++ Update-sandbox-customs.py
@@ -97,13 +97,14 @@ for app_name in app_list:
             sandboxes1 = response['_embedded']
             sandboxes2 = sandboxes1['sandboxes']
             sandbox_guid = sandboxes2[i]['guid']
+            print(sandbox_guid)
             i += 1
         except: 
             print("\nCould not find Sandbox Details")
             sys.exit(1)
 
         #Update Schedule of existing DA Job
-        print(sandbox_guid)
+        #print(sandbox_guid)
         res = prepared_request('PUT', 'https://api.veracode.com/appsec/v1/applications/' + app_guid + '/sandboxes/' + sandbox_guid, json=data)
         print(res.status_code)
         try:
