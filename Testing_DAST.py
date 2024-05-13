67b7b2bf0bf970cc1dc52c24e126a7d5a7195cfe
Samab2024 <158163459+Samab2024@users.noreply.github.com>
2024-05-13 13:33:07 +0530

    Update Testing_DAST.py

--- Testing_DAST.py
+++ Testing_DAST.py
@@ -81,5 +81,5 @@ if __name__ == "__main__":
     log.setLevel(logging.INFO)
     log.setLevel(logging.DEBUG)
     a = DynamicAnalysis()
-    sscan_name='Findings DAST'
+    scan_name='Findings DAST'
     scans =a.scan_now(scan_name)
