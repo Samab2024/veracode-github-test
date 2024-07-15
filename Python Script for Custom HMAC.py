167ca70f1502a5ab959d78349780647e57dd1a6f
Samab2024 <158163459+Samab2024@users.noreply.github.com>
2024-07-15 16:21:07 +0530

    Update Python Script for Custom HMAC.py

--- Python Script for Custom HMAC.py
+++ Python Script for Custom HMAC.py
@@ -58,7 +58,7 @@ print("\nLooking for Applications accessible to the profile\n")
 #Retrieve Application_data name
 res = prepared_request('GET', 'https://api.veracode.com/appsec/v1/applications')
 response = res.json()
-print(response)
+#print(response)
 records = len(response)
 app_id=response['_embedded']['applications'][0]['id']
 app_guid=response['_embedded']['applications'][0]['guid']
@@ -70,8 +70,8 @@ try:
     for x in range(0, records):
         app_id=response['_embedded']['applications'][x]['id']
         app_guid=response['_embedded']['applications'][x]['guid']
-        Policy_Name=response['_embedded']['applications'][x]['profile']['policies']['name']
-        Policy_Check_Status=response['_embedded']['applications'][x]['profile']['policies']['policy_compliance_status']
+        Policy_Name=response['_embedded']['applications'][x]['profile']['policies'][0]['name']
+        Policy_Check_Status=response['_embedded']['applications'][x]['profile']['policies'][0]['policy_compliance_status']
         Last_Policy_Check_Date=response['_embedded']['applications'][x]['last_policy_compliance_check_date']
         print(app_id + '|' + app_guid + '|' + Policy_Name + '|' + Policy_Check_Status + '|' + Last_Policy_Check_Date)
         x=x+1
