1c7c068021d90d6ce36ebb0aec8151daadaa36f3
Samab2024 <158163459+Samab2024@users.noreply.github.com>
2024-07-15 15:13:07 +0530

    Update Python Script for Custom HMAC.py

--- Python Script for Custom HMAC.py
+++ Python Script for Custom HMAC.py
@@ -55,14 +55,6 @@ def prepared_request(method, end_point, json=None, query=None, file=None):
 # code above this line is reusable for all/most API calls
 
 var template = `
-<style type="text/css">
-    .tftable {font-size:14px;color:#333333;width:100%;border-width: 1px;border-color: #87ceeb;border-collapse: collapse;}
-    .tftable th {font-size:18px;background-color:#87ceeb;border-width: 1px;padding: 8px;border-style: solid;border-color: #87ceeb;text-align:left;}
-    .tftable tr {background-color:#ffffff;}
-    .tftable td {font-size:14px;border-width: 1px;padding: 8px;border-style: solid;border-color: #87ceeb;}
-    .tftable tr:hover {background-color:#e0ffff;}
-</style>
-
 <table class="tftable" border="1">
     <tr>
         <th>App ID</th>
